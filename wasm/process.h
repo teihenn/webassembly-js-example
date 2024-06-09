3eade79903a985ff927e5e9ceb51f1990ff8f5c7
yo1956 <hm.hr.yossy@gmail.com>
2024-06-09 22:26:45 +0900

    Add comment to process.h

--- wasm/process.h
+++ wasm/process.h
@@ -10,6 +10,11 @@
 extern "C" {
 #endif
 
+// The EMSCRIPTEN_KEEPALIVE macro is used to prevent the Emscripten compiler
+// from eliminating or mangling the names of functions during optimization.
+// This ensures that the specified functions remain accessible and callable from
+// JavaScript, even after the aggressive optimizations typically performed
+// during the WebAssembly compilation process.
 float* EMSCRIPTEN_KEEPALIVE process(int* a,
                                     int num_of_a,
                                     float* b,
