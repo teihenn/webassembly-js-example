94a56b1b7f1c078c07307d270e7d6528196c5e0c
yo1956 <hm.hr.yossy@gmail.com>
2024-06-09 22:26:52 +0900

    Add comment to process.cpp

--- wasm/process.cpp
+++ wasm/process.cpp
@@ -2,6 +2,14 @@
 #include <stdio.h>
 #include "dbg.h"
 
+// Without "extern "C"", attempting to call process() from js/process.js would
+// result in a TypeError: process is not a function
+// Without "extern "C"", attempting to call process() from js/process.js would
+// result in a TypeError: process is not a function. This is because C++
+// compilers apply name mangling to function names, which changes their names in
+// the compiled output. Using "extern "C"" prevents name mangling, ensuring the
+// function name remains consistent and recognizable when called from other
+// languages like JavaScript.
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -13,7 +21,7 @@ float* EMSCRIPTEN_KEEPALIVE process(int* a,
   static float ret[3] = {0., 0., 0.};
   for (int i = 0; i < num_of_a; i++) {
     // In WASM, functions like printf cannot be used, so for debugging purposes,
-    // values are stored in memory and then retrieved from JS.
+    // values are stored in memory and then retrieved from JavaScript.
 #ifdef DEBUG
     char buf[256];
     snprintf(buf, sizeof(buf), ">> [%d] = %d\n", i, a[i]);
